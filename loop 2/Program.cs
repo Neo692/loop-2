db2f6096555a8911441a0e3531b0a801baed29a1
nehy06001 <nehy06001@E-PW05CGQV>
2024-12-13 08:24:15 +0100

    Add project files.

---
+++ loop 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace loop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            int tal = int.Parse(Console.ReadLine());
+            while (tal != 0)
+            {
+                Console.WriteLine("Skriv in ett annat heltal");
+                tal = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
